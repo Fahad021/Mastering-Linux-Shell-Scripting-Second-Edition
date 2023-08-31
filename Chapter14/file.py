244c7306645928725a0537b6d2171b4d6c1cfbed
Sourcery AI <>
2023-08-31 19:36:43 +0000

    'Refactored by Sourcery'

--- Chapter14/file.py
+++ Chapter14/file.py
@@ -3,11 +3,6 @@ import sys
 count = len(sys.argv)
 name = ''
 
-if ( count == 1 ):
-    name = input("Enter a name: ") 
-else:
-    name = sys.argv[1]
-
-log = open("/tmp/script.log","a")
-log.write("Hello " + name + "\n")
-log.close()
+name = input("Enter a name: ") if ( count == 1 ) else sys.argv[1]
+with open("/tmp/script.log","a") as log:
+    log.write(f"Hello {name}" + "\n")
